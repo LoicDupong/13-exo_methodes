f6924c0f10d265ffb32d9b997d40e5b8ecc75de4
Loic Dupong <loic@dupong.be>
2025-05-21 15:59:04 +0200

    Init- Loïc

--- js/main.js
+++ js/main.js
@@ -43,3 +43,66 @@
 // - Player.js
 // - Game.js
 // - index.js (ou app.js pour lancement)
+
+
+class Game{
+    constructor(players, distribution){
+        this.players = players;
+        this.distribution = distribution;
+        this.deck = [];
+        this.pile = [];
+    }
+    generateDeck(){
+        const signes = ['♠', '♥', '♦', '♣'];
+        const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+        // for (let signe of signes) {
+        //     for (let value of values) {
+        //         const nom = ${value} de ${signe};
+        //         const effet = effetsFonctions[value] || null; // effet uniquement pour les cartes spéciales
+        //         const carte = new Card(nom, value, signe, effet);
+        //         deck.push(carte);
+        //     }
+        // }
+    }
+    distributionCards(){
+        for (let player of this.players) {
+            player.hand = this.deck.splice(0, 3);
+            player.faceUp = this.deck.splice(0, 3);
+            player.faceDown = this.deck.splice(0, 3);
+        } 
+    }
+    
+    turnEnd(){
+        return `Fin du tour`;
+    }
+}
+
+class Card {
+    constructor(name, value, symbol, effect) {
+        this.name = name;
+        this.value = value;
+        this.symbol = symbol;
+        this.effect = effect;
+    }
+}
+
+const effetsFonctions = {
+    "2": (game) => {/* peut poser sur tout */},
+    "3": (game) => {/* copie la carte précédente */},
+    "7": (game) => {/* limite à < 7 */},
+    "8": (game) => { game.skipNextPlayer(); },
+    "10": (game) => { game.burnPile(); game.replay(); },
+    "As": (game) => { game.attack(); }
+  };
+
+const getEffet = (name) => effetsFonctions[name] || null;
+for (let sym of symbols) {
+  for (let val of values) {
+    pioche.push(new Card(val.name, val.value, sym, getEffet(val.name)));
+  }
+}
+
+// playMultipleCards(cards){
+    //     return cards.every(c => c.value === cards[0].value);
+    // }
